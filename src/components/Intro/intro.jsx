affc1528b1d324471ca0ad064d3260acf2c3960b
rammohanmiryala <rammohanmiryala@hotmail.com>
2022-06-14 23:38:28 +0930

    added intro responsive

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,62 +1,53 @@
-import React, { Component } from "react";
+import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./intro.css";
+import "./typewriter"
 
-
-const MyComponent = () => {
-  const handleType = (count: number) => {
-    // access word count number
-    console.log(count);
-  };
-};
-
-const handleDone = () => {
-  console.log(`Done after 5 loops!`);
-};
-
-export default class intro extends Component {
-  render() {
-    return (
-      <>
-        <Container
-          expand="lg"
-          className="justify-content-md-center mainbodydiv"
-        >
-          <Col className="bodytag" md={{ span: 2, offset: 2 }}>
-            &lt;body&gt;
-          </Col>
-          <Col className="justify-content-md-center introbody" lg="8">
-            <Col className="justify-content-md-center tagtext" lg="8">
-              <Col className="wavetext">
-                <span className="hitext">Hi,</span>
-                <span className="wave">👋</span> my name is
-              </Col>
-              <Col className="nameletter">
-                <Col className="nameletterdiv">
-                  <span className="Rletter">R</span>
-                  <span className="amletter">am</span>
-                  <span className="dotsletter">..</span>
-                </Col>
-              </Col>
-              <Row className="designertext App">               
-                <Col lg="auto">Ui/Ux Designer</Col>
-                <Col lg="auto">& </Col>
-                <Col lg="auto">Web Developer &#x7B; &#xFF5D;;</Col>
-              </Row>
-              <Col className="aboutmetext" lg="12">
-                I design and develop services for customers of all sizes,
-                specializing in creating stylish, appealing websites, web
-                services and online stores.
+const Intro = () => {
+  return (
+    <>
+      <Container expand="lg" className="justify-content-md-center mainbodydiv">
+        <Col className="bodytag" md={{ span: 2, offset: 2 }}>
+          &lt;body&gt;
+        </Col>
+        <Col className="justify-content-md-center introbody" lg="8">
+          <Col className="justify-content-md-center tagtext" lg="10">
+            <Col className="wavetext">
+              <span className="hitext">Hi,</span>
+              <span className="wave">👋</span> <span className="mynameis">my name is </span>
+            </Col>
+            <Col className="nameletter">
+              <Col className="nameletterdiv">
+                <span className="Rletter">R</span>
+                <span className="amletter">am</span>
+                <span className="dotsletter">..</span>
               </Col>
             </Col>
+            <Col className="designertext ">
+              <span>I'm  </span> <span></span><span
+                href=""
+                className="typewrite"
+                data-period="2000"
+                data-type='[  "Creative.", " a Ui/Ux Designer.", "a Developer &#123; &#125;." ]'
+              >
+                <span class="wrap"></span>
+              </span>
+            </Col>
+            <Col className="aboutmetext" lg="11">
+              I design and develop services for customers of all sizes,
+              specializing in creating stylish, appealing websites, web services
+              and online stores.
+            </Col>
           </Col>
-          <Col className="bodytag" md={{ span: 2, offset: 2 }}>
-            &lt;body&gt;
-          </Col>
-        </Container>
-      </>
-    );
-  }
-}
+        </Col>
+        <Col className="bodytag" md={{ span: 2, offset: 2 }}>
+          &lt;body&gt;
+        </Col>
+      </Container>
+    </>
+  );
+};
+
+export default Intro;
